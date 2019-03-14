b9e7fe9fb0f0f30fa83d5a7ed42329cd0d94f412
perfunctory98 <1594406903@qq.com>
2019-03-14 15:17:51 +0800

    Site updated: 2019-03-14 15:17:49

--- js/count.js
+++ js/count.js
@@ -1,3 +1,21 @@
 var mycopyright=document.getElementsByClassName("src-components-footer-footer---s2---2E3yO");
-var copyr=mycopyright[0].innerHTML;
-mycopyright[0].innerHTML=(copyr+'</br><script async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script> <span id="busuanzi_container_site_pv">本站总访问量<span id="busuanzi_value_site_pv"></span>次</span>')
+function insertAfter(newElement, targentElement) {
+        var parent = targentElement.parentNode;
+        if (parent.lastChild == targentElement) {
+            parent.appendChild(newElement);
+        } else {
+            parent.insertBefore(newElement, targentElement.nextSibling)
+        }
+}
+var script=document.createElement("script");
+script.type="text/javascript";
+script.src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js";
+document.getElementsByTagName('head')[0].appendChild(script); 
+
+mycopyright[0].style.whiteSpace="pre-wrap";
+var text1=mycopyright[0].innerHTML;
+mycopyright[0].innerHTML=text1+'\n'+'本站总访问量<span id="busuanzi_value_site_pv"></span>次'+'  您是第<span id="busuanzi_value_site_uv"></span>位访客';
+
+
+
+ 
